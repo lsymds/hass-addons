029a8816586567dbc36c061104f80fe314c791fc
Liam Symonds <47109131+lsymds@users.noreply.github.com>
2025-06-10 23:25:13 +0100

    fix(ukbincollectiondata): Stop showing state as timestamp

--- ukbincollectiondata/rootfs/ukbincollectiondata/ukbincollectiondata.py
+++ ukbincollectiondata/rootfs/ukbincollectiondata/ukbincollectiondata.py
@@ -163,7 +163,6 @@ def update_sensor(sensor_id, state):
         "friendly_name": friendly_name,
         "icon": "mdi:trash-can",
         "device_class": "timestamp",
-        "state_class": "measurement",
         "last_updated": datetime.now().isoformat(),
     }
     attributes.update(state)  # Add all the state data as attributes
